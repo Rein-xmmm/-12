1993f242420eb2c93497d9f0e5b78b3b8a70d531
Rein-xmmm <72437331+Rein-xmmm@users.noreply.github.com>
2020-12-18 19:42:08 +0900

    Initial commit

---
+++ Math и Random/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_и_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("K=3t^2 +3sinx^3 +9.2");
+            Random rand = new Random();
+            int t = rand.Next(0, 99);
+            int x = rand.Next(0, 99);
+
+            Double K = 3 * Math.Pow(t, 2) + 3 * Math.Sin( Math.Pow(x, 3)) + 9.2;
+            Console.WriteLine($"t= {t}");
+            Console.WriteLine($"x= {x} ");
+            Console.WriteLine($"K={K}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
